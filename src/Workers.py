bc6ad6bab12b02623f221b404e10c87f060d8980
Aybrl <contact@aybrl.com>
2023-01-23 13:23:24 +0100

    Get flights and Write to Stream

--- src/Workers.py
+++ src/Workers.py
@@ -8,27 +8,58 @@ api_helper = {
     "token": ""
 }
 
-airlines = ["AS", "G4", "AA", "XP", "MX", "DL", "2D", "F9", "HA", "B6", "WN", "NK", "SY", "UA"]
-
 STREAM_NAME = "episen-flight-data-stream"
 REGION = "us-east-1"
-DELIVERY_STREAM_NAME = ""
-STREAM_SCHEMA=""
+DELIVERY_STREAM_NAME = "KDS-S3-85YtU"
+STREAM_SCHEMA = "arn:aws:firehose:us-east-1:921510072347:deliverystream/KDS-S3-85YtU"
+
+airlines = ["AS", "G4", "AA", "XP", "MX", "DL", "2D", "F9", "HA", "B6", "WN", "NK", "SY", "UA"]
+airports = ["ATL", "LAX", "ORD", "DFW", "DEN", "JFK", "SFO", "SEA", "MCO", "LAS"]
 
 
 def generate_stream():
+    # US Airlines Flights
     put_data_aws_stream(get_us_airlines_flights())
+    # US Airlines Routes
+    get_put_us_airlines_routes()
+    # US Top Airports Departures
+    get_put_us_top_airports_data("departure")
+    # US Top Airports Arrivals
+    get_put_us_top_airports_data("arrival")
 
 
 def get_us_airlines_flights():
     us_airlines_flights = []
-    for airline in airlines :
+    for airline in airlines:
         url = f"{api_helper.get('base_url')}/flights?key={api_helper.get('token')}&airlineIata={airline}"
         flight_data = make_request(url)
         data = {f"{airline}": flight_data}
         print(data)
         us_airlines_flights.append(data)
     return us_airlines_flights
+
+
+def get_put_us_airlines_routes():
+    us_airlines_routes = []
+    for airline in airlines:
+        url = f"{api_helper.get('base_url')}/routes?key={api_helper.get('token')}&airlineIata={airline}"
+        route_data = make_request(url)
+        data = {f"{airline}": route_data}
+        print(data)
+        put_data_aws_stream(data)
+        us_airlines_routes.append(data)
+    return us_airlines_routes
+
+
+def get_put_us_top_airports_data(type):
+    us_airports_data = []
+    for airport in airports:
+        url = f"{api_helper.get('base_url')}/timetable?key={api_helper.get('token')}&iataCode={airport}&type={type}"
+        airport_data = make_request(url)
+        data = {f"{airport}": airport_data}
+        print(data)
+        us_airports_data.append(data)
+    return us_airports_data
 
 
 def put_data_aws_stream(data):
@@ -42,8 +73,8 @@ def put_data_aws_stream(data):
 # Helpers
 def make_request(endpoint):
     headers = {
-        #"'Accept': 'application/vnd.github+json',
-        #'Authorization': f'Bearer {api_helper.get("token")}',
+        # "'Accept': 'application/vnd.github+json',
+        # 'Authorization': f'Bearer {api_helper.get("token")}',
     }
 
     response = requests.get(endpoint, headers=headers)
@@ -61,5 +92,3 @@ def make_request(endpoint):
 
     print(response.text)
     return False
-
-
